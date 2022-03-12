4fc57a598527da44d11c13fdb439e40cd3eeafd0
Vian Alfalah <vianalfalah248@gmail.com>
2022-03-12 16:33:18 +0700

    no message

--- src/component/InputTodo/index.js
+++ src/component/InputTodo/index.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { editTodo, createTodo } from "../../redux/action";
 import { useDispatch } from "react-redux";
 export default function Update({ data, modal, modalDesk, setAdd }) {
   const dispatch = useDispatch();
+  const [error, setError] = useState({
+    title: "",
+    description: "",
+  });
   const [formValue, setFormValue] = useState(
     setAdd
       ? {
@@ -31,6 +35,26 @@ export default function Update({ data, modal, modalDesk, setAdd }) {
       modal.set(false);
     }
   };
+
+  useEffect(() => {
+    if (formValue.title) {
+      if (formValue.title.length < 6) {
+        setError({ ...error, title: "Must be 6 - 20 Characters" });
+      } else {
+        setError({ ...error, title: "" });
+      }
+    }
+  }, [formValue.title]);
+  useEffect(() => {
+    if (formValue.description) {
+      if (formValue.description.length < 8) {
+        setError({ ...error, description: "Must be 8 - 100 Characters" });
+      } else {
+        setError({ ...error, description: "" });
+      }
+    }
+  }, [formValue.description]);
+
   return (
     <div>
       <h5 className="text-center p-2">{setAdd ? "New To Do" : "Edit To Do"}</h5>
@@ -40,13 +64,19 @@ export default function Update({ data, modal, modalDesk, setAdd }) {
         name="title"
         type="text"
         value={formValue?.title}
+        required
+        maxLength={20}
       />
+      {error.title && <p className="p-2 red">{error.title}</p>}
       <textarea
         onChange={(e) => handleChange(e)}
         id="description"
         name="description"
         value={formValue?.description}
+        required
+        maxLength={100}
       />
+      {error.description && <p className="p-2 red">{error.description}</p>}
       {!setAdd && (
         <select
           onChange={(e) => handleChange(e)}
@@ -59,7 +89,16 @@ export default function Update({ data, modal, modalDesk, setAdd }) {
         </select>
       )}
 
-      <button className="text-center" onClick={handleSubmit}>
+      <button
+        className="text-center"
+        onClick={handleSubmit}
+        disabled={
+          error.description ||
+          error.title ||
+          !formValue.title ||
+          !formValue.description
+        }
+      >
         oke
       </button>
     </div>
